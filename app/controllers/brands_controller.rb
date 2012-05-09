f25f1ccaf831ad54e3832089f5a7f1001ee9bfb0
Joel <joel@redhorn.se>
2012-05-09 23:58:34 +0200

    Add basic brand functionality and admin menu

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,45 @@
 class BrandsController < ApplicationController
+
+  def index
+    @brands = Brand.all
+  end
+
+  def show
+    @brand = Brand.find_by_uid(params[:id])
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def edit
+    @brand = Brand.find_by_uid(params[:id])
+  end
+
+  def update
+    @brand = Brand.find_by_uid(params[:id])
+    if @brand.update_attributes(params[:brand])
+      flash[:success] = "Brand updated"
+      redirect_to @brand
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @brand = Brand.new(params[:brand])
+    if @brand.save
+      flash[:success] = "Created brand: #{@brand.name}"
+      redirect_to @brand
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    brand = Brand.find_by_uid(params[:id]).destroy
+    flash[:success] = "Brand '#{brand.name}' destroyed."
+    redirect_to brands_path
+  end
+
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,24 @@
 class Brand < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :uid, :active, :collecting, :elo, :wr
+
+  before_save :cleanup_name
+  before_save :create_uid
+
+  validates :name, presence: true
+
+  def to_param
+    "#{self.uid.parameterize}"
+  end
+
+  private
+
+    def cleanup_name
+      self.name = self.name.gsub(/\s\s+/, ' ')
+    end
+
+    def create_uid
+      self.uid = name.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/, '').downcase.to_s
+      self.uid = self.uid.gsub(/ /, '_')
+    end
+
 end
