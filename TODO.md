e4bedb4043312a57d2fc39cbfbab0dcb0ebdd4d8
Joel <joel@redhorn.se>
2012-05-10 23:14:01 +0200

    Add tag cloud to brands

--- TODO.md
+++ TODO.md
@@ -6,4 +6,4 @@
 #### Logotypes
 
 * Logotypes should fit in a container with fixed height to avoid rearranging page elements after they load
-
+* Store logotypes on S3
--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -4,4 +4,27 @@ module BrandsHelper
     image_tag("logotypes/#{brand.uid}_logotype.png", options)
   end
 
+  def tag_cloud_for(brand)
+    # Put all unique tags in a hash and count them individually
+    tags = {}
+    brand.tags.shuffle.each do |t|
+      key = t.value
+      unless tags.include?(key)
+        tags[key] = 1 unless key.empty?
+      else
+        tags[key] += 1
+      end
+    end
+    # Create tag cloud markup
+    content_tag(:div, create_cloud(tags), class: "tag-cloud").html_safe
+  end
+
+  def create_cloud(tags)
+    cloud = ''
+    tags.each do |key, value|
+      cloud += content_tag(:span, key, style: "font-size: #{( 1 + (value - 1) * 0.4 )}em")
+    end
+    cloud.html_safe
+  end
+
 end
