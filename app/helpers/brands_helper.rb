7a78fbeca496ac66a022819f966eb87993f15f53
Joel <joel@redhorn.se>
2012-05-11 00:32:11 +0200

    Add center weighted array sorting for tag clouds

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -7,7 +7,7 @@ module BrandsHelper
   def tag_cloud_for(brand)
     # Put all unique tags in a hash and count them individually
     tags = {}
-    brand.tags.shuffle.each do |t|
+    brand.tags.each do |t|
       key = t.value
       unless tags.include?(key)
         tags[key] = 1 unless key.empty?
@@ -15,8 +15,12 @@ module BrandsHelper
         tags[key] += 1
       end
     end
-    # Create tag cloud markup
-    content_tag(:div, create_cloud(tags), class: "tag-cloud").html_safe
+
+    # Convert tags hash to array and sort it on tag counts
+    tags = tags.to_a.sort { |a, b| a[1] <=> b[1] }
+
+    # Create tag cloud markup from a center weighted array
+    content_tag(:div, create_cloud(center_weight(tags)), class: "tag-cloud").html_safe
   end
 
   def create_cloud(tags)
@@ -25,6 +29,30 @@ module BrandsHelper
       cloud += content_tag(:span, key, style: "font-size: #{( 1 + (value - 1) * 0.4 )}em")
     end
     cloud.html_safe
+  end
+
+  # Takes an array (sorted from lowest to highest), and rearranges it to have the largest
+  # value in the center and the rest going down to the right and left.
+  # Example: [0, 1, 2, 3, 4, 5, 6] --> [1, 3, 5, 6, 4, 2, 0]
+  def center_weight(a)
+    wa = []
+
+    i = left = 0
+    right = a.length - 1
+
+    a.each do |item|
+      if i.even?
+        wa[right] = item
+        right = right - 1
+      else
+        wa[left] = item
+        left = left + 1
+      end
+
+      i = i + 1
+    end
+
+    return wa
   end
 
 end
